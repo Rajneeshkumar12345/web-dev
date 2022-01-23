864cffb833537d4779ece244757d5410dc0d38a9
Rajneeshkumar12345 <rkv7861999@gmail.com>
2022-01-23 17:14:00 +0530

    add

---
+++ 1_variable.js/5_Js_Concepts/RefDataTypes.js
@@ -0,0 +1,34 @@
+// let arr = [1,2,2]
+// console.log(arr,typeof(arr))
+
+
+// let obj = {
+//     name : 'rajneeesh',
+//     age : 21
+// }
+// console.log(obj, typeof(obj))
+
+
+
+// function greet(){
+//     console.log('hello')
+// }
+// console.log(greet, typeof(greet))
+
+
+
+let firstPerson = {
+      Name : 'Rajneessh',
+      age : 21
+}
+
+let secondPerson = firstPerson
+
+firstPerson.name  = 'Hanuman'
+
+
+console.log(firstPerson)
+
+console.log(secondPerson)
+
+
