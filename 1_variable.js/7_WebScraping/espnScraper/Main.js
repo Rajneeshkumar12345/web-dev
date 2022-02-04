d1957c058368f55876863afe156d14c5de2c56b3
Rajneeshkumar12345 <rkv7861999@gmail.com>
2022-02-04 13:37:52 +0530

    added

--- 1_variable.js/7_WebScraping/espnScraper/Main.js
+++ 1_variable.js/7_WebScraping/espnScraper/Main.js
@@ -10,6 +10,48 @@ function cb(err, response, html) {
   if (err) {
     console.error(err);
   } else {
-    console.log(html);
+    extractLink(html);
   }
+}
+
+function extractLink(html) {
+    let $ = cheerio.load(html);
+    let anchorElem = $('a[data-hover="View All Results"]');
+  
+    let link = anchorElem.attr("href");
+  
+    //console.log(link)
+  
+    let fullLink = "https://www.espncricinfo.com" + link;
+   // console.log(fullLink)
+
+
+    
+  getAllMatchLink(fullLink)
+}
+
+function getAllMatchLink(uri){
+        request(uri , function(error , response , html){
+               if(error){
+                      console.log(error)
+               }
+               else{
+                      extractAllLink(html)
+               }
+        } )
+}
+
+
+function extractAllLink(html){
+   let $ = cheerio.load(html)
+
+   let scoreCardArr = $('a[data-hover="Scorecard"]')
+
+   for(let i=0 ; i<scoreCardArr.length ; i++){
+          let link = $(scoreCardArr[i]).attr('href')
+          let fullLink = 'https://www.espncricinfo.com/'+link
+          console.log(fullLink)
+   }
+
+
 }
